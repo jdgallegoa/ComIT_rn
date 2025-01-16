f770cacf8a62be27c693ec2bdb78ad3d602e4939
unknown <jdgallegoa@gmail.com>
2025-01-15 19:01:45 -0500

    Ejercicio 2 de JS por: Juan David Gallego

---
+++ ej2.js
@@ -0,0 +1,11 @@
+var nombre;
+var apellido;
+var edad;
+var fechaDeNacimiento;
+var direccion;
+nombre = "Juan";
+apellido = "Gallego";
+edad = 27;
+fechaDeNacimiento = '1997-07-26';
+direccion = 'Cra 4A #4-38'
+
